fb5d9acf291c273946a5067658d00dd132bfda43
neosmiles <eselen.aig@gmail.com>
2024-06-09 14:19:47 +0100

    update

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -1,12 +1,14 @@
 using BlazorApp;
 using BlazorApp.Services;
-
+using Syncfusion.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXleeXRdQmddVEFwX0Y=");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<HeroImageService>();
